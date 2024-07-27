a63da3d891ad537861ac599adb9f75a20975c817
yuyawk <riotintacos@gmail.com>
2024-07-28 02:35:42 +0900

    mkdir rootfs

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -58,7 +58,12 @@ if [ -d "${OUTDIR}/rootfs" ]; then
     sudo rm  -rf "${OUTDIR}/rootfs"
 fi
 
-# TODO: Create necessary base directories
+# Create necessary base directories
+mkdir -p "${OUTDIR}/rootfs"
+cd "${OUTDIR}/rootfs"
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "${OUTDIR}"
 if [ ! -d "${OUTDIR}/busybox" ]; then
