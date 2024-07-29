4179aeb95024175847a2a4178ccb4b7543622e92
yuyawk <riotintacos@gmail.com>
2024-07-30 01:04:37 +0900

    modify comment

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -40,7 +40,7 @@ void *threadfunc(void *thread_param)
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex, int wait_to_obtain_ms, int wait_to_release_ms)
 {
     /**
-     * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
+     * Allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
      * using threadfunc() as entry point.
      *
      * return true if successful.
