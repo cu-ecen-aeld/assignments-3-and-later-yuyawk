f67e42dbe2b89b0e45ceb60bccf4567fffd428fa
yuyawk <riotintacos@gmail.com>
2024-08-04 00:19:36 +0900

    modify finder-test.sh for assignment 4

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,7 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+username="$(cat /etc/finder-app/conf/username.txt)"
 
 if [ $# -lt 3 ]
 then
@@ -31,19 +31,19 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 
 rm -rf "${WRITEDIR}"
 
-# create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
+# create ${WRITEDIR} if not assignment1
+assignment="$(cat /etc/finder-app/conf/assignment.txt)"
 
 if [ $assignment != 'assignment1' ]
 then
-	mkdir -p "$WRITEDIR"
+	mkdir -p "${WRITEDIR}"
 
 	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
 	#The quotes signify that the entire string in WRITEDIR is a single string.
 	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
-	if [ -d "$WRITEDIR" ]
+	if [ -d "${WRITEDIR}" ]
 	then
-		echo "$WRITEDIR created"
+		echo "${WRITEDIR} created"
 	else
 		exit 1
 	fi
@@ -52,12 +52,13 @@ fi
 #make clean
 #make
 
-for i in $( seq 1 $NUMFILES)
+cd "$(dirname $0)"
+for i in $(seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	./writer "${WRITEDIR}/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(./finder.sh "${WRITEDIR}" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
@@ -65,9 +66,9 @@ rm -rf /tmp/aeld-data
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
-	echo "success"
+	echo "success" > /tmp/assignment4-result.txt
 	exit 0
 else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found" > /tmp/assignment4-result.txt
 	exit 1
 fi
